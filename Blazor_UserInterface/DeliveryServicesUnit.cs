91f9faadae2bd53ec4b235d6a5da3c7ede047376
Konstantin Batov <eckumoc@gmail.com>
2024-08-05 00:32:06 +0300

    Не работает

---
+++ Blazor_UserInterface/DeliveryServicesUnit.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Console_AuthModel.AuthorizationServices;
+
+using Console_BlazorApp.AppUnits;
+using Console_BlazorApp.AppUnits.AuthorizationTests;
+using Console_BlazorApp.AppUnits.DeliveryTests;
+
+namespace Console_BlazorApp
+{
+    [Label("Тестирование приложения для продажи и доставки авто-товаров.")]
+    public class DeliveryServicesUnit : TestingUnit
+    {
+        public DeliveryServicesUnit( IServiceProvider provider = null )
+        {
+
+
+            if( provider is null )
+            {
+                provider = AppProviderService.GetSingletonInstance();
+            }
+            this.Append(new HolderServicesTestUnit(provider));
+            this.Append(new AuthorizationUnit(provider));
+            
+            this.Append(new CustomerOrderCheckoutUnit(provider));
+            this.Append(new TransportOrderDeliveryUnit(provider));
+            this.Append(new ReservationOrderCheckoutUnit(provider));
+        }
+
+        
+    }
+}
